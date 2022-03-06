6859552c28e29574b6c9c8ee7e53878965467ae7
Pawel Dudko <paweldudko@outlook.com>
2022-03-06 10:08:42 +0100

    day2_task1 solution

---
+++ src/pl/sdacademy/java/basic/exercises/day2/Task1.java
@@ -0,0 +1,31 @@
+package pl.sdacademy.java.basic.exercises.day2;
+
+public class Task1 {
+    private static final char DEFAULT_CHAR = '\u0000';
+
+    public static void main(String[] args) {
+        String input = "domek   ";
+        char result = getLastChar(input);
+        System.out.println("Original char: " + input);
+        System.out.println("Last char: " + result);
+
+        input = null;
+        result = getLastChar(input);
+        System.out.println("Original char: " + input);
+        System.out.println("Last char: " + result);
+
+        input = "  ";
+        result = getLastChar(input);
+        System.out.println("Original char: " + input);
+        System.out.println("Last char: " + result);
+    }
+
+    private static char getLastChar(String input) {
+        if(input != null && !input.isBlank()) {
+            String inputWithoutTrim = input.trim();
+            int lastPosition = inputWithoutTrim.length() - 1;
+            return inputWithoutTrim.charAt(lastPosition);
+        }
+        return DEFAULT_CHAR;
+    }
+}
