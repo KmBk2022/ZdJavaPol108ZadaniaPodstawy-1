087b212398fdfe29c38732a1b1a9591070df08e5
Pawel Dudko <paweldudko@outlook.com>
2022-03-06 20:21:31 +0100

    completed day2

--- src/pl/sdacademy/java/basic/exercises/day2/Task15.java
+++ src/pl/sdacademy/java/basic/exercises/day2/Task15.java
@@ -5,26 +5,26 @@ public class Task15 {
     public static void main(String[] args) {
         String[] firstArray = {"Ala", "ma", "kota"};
         String[] secondArray = {"Ala", "ma", "kota"};
-        System.out.println(areEquals(firstArray, secondArray));
+        System.out.println(areEqual(firstArray, secondArray));
 
         String[] firstArray1 = {"Ala", "ma", "kot"};
         String[] secondArray1 = {"Ala", "ma", "kota"};
-        System.out.println(areEquals(firstArray1, secondArray1));
+        System.out.println(areEqual(firstArray1, secondArray1));
 
         String[] firstArray2 = {"Ala", null, "kota"};
         String[] secondArray2 = {"Ala", "ma", "kota"};
-        System.out.println(areEquals(firstArray2, secondArray2));
+        System.out.println(areEqual(firstArray2, secondArray2));
 
         String[] firstArray3 = {"Ala", "ma", "kota"};
         String[] secondArray3 = {"Ala", null, "kota"};
-        System.out.println(areEquals(firstArray3, secondArray3));
+        System.out.println(areEqual(firstArray3, secondArray3));
 
         String[] firstArray4 = {"Ala", null, "kota"};
         String[] secondArray4 = {"Ala", null, "kota"};
-        System.out.println(areEquals(firstArray4, secondArray4));
+        System.out.println(areEqual(firstArray4, secondArray4));
     }
 
-    private static boolean areEquals(String[] firstArray, String[] secondArray) {
+    private static boolean areEqual(String[] firstArray, String[] secondArray) {
         if(firstArray.length == secondArray.length) {
             for(int i = 0; i < firstArray.length; i++) {
                 String firstArrElement = firstArray[i];
